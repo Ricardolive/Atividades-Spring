dad15eb46852927dd030cc8ca9dec240302dd91b
ricardo <ricardoliveira50@hotmail.com>
2021-05-27 20:36:24 -0300

    Blog Pessoal V3

--- blogPessoal/src/main/java/br/com/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/br/com/generation/blogPessoal/controller/PostagemController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+
 
 import br.com.generation.blogPessoal.repository.PostagemRepository;
 import br.com.generation.blogPessoal.model.Postagem;
